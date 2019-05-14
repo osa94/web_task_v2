bd189182be42a74c9f4a53d003961efb4b6cb40c
Oskar <oskarkazmierczak1994@gmail.com>
2019-05-14 19:55:54 +0200

    Create TeacherViewSet
    Created TeacherViewSet in teachers/views

--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from teachers.models import Teacher
+from teachers.serializers import TeacherSerializer
 
-# Create your views here.
+
+class TeacherViewSet(viewsets.ModelViewSet):
+
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
