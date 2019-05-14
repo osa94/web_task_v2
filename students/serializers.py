276d864fc07c4d6844535541d27d5a47a1778d54
Oskar <oskarkazmierczak1994@gmail.com>
2019-05-14 20:53:16 +0200

    Create StudentSerializer
    Created StudentSerializer in students/serializers

---
+++ students/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from students.models import Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='username.username', )
+    teachers = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Student
+        fields = ('id', 'username', 'teachers',)
