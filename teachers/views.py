46f071a86ee9d8a730e1f141ea19836a03c8616b
Oskar <oskarkazmierczak1994@gmail.com>
2019-05-14 20:00:18 +0200

    Create permission ReadOnly
    Created permission ReadOnly in teachers/permissions and include this in teachers/views in TeacherViewSet

--- teachers/views.py
+++ teachers/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
 from teachers.models import Teacher
 from teachers.serializers import TeacherSerializer
+from teachers.permissions import ReadOnly
 
 
 class TeacherViewSet(viewsets.ModelViewSet):
 
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
-
+    permission_classes = (ReadOnly,)
