e7bf95ab089668d1b5e74cdc0876008d82211676
Oskar <oskarkazmierczak1994@gmail.com>
2019-05-14 22:37:11 +0200

    Create TaskViewSet and make filter in TaskSerializer
    Created TaskViewSet in tasks/views and let teacher create tasks for theirs exams (filter is in tasks/serializers)

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from exams.models import Exam
 from tasks.models import Task
+from teachers.models import Teacher
 
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    answers = serializers.StringRelatedField(many=True, read_only=True)
     owner = serializers.ReadOnlyField(source='owner.username.username')
     exam = serializers.PrimaryKeyRelatedField(queryset=Exam.objects.all())
 
+    def __init__(self, *args, **kwargs):
+        super(TaskSerializer, self).__init__(*args, **kwargs)
+        current_user = self.context['request'].user
+        teacher = Teacher.objects.get(username=current_user)
+        self.fields['exam'].queryset = Exam.objects.filter(owner=teacher)
+
     class Meta:
         model = Task
-        fields = ('id', 'owner',  'exam', 'content', 'max_points', )
+        fields = ('id', 'owner',  'exam', 'content', 'answers', 'max_points', )
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from tasks.models import Task
+from teachers.models import Teacher
+from exams.models import Exam
+from tasks.serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def get_current_teacher(self):
+        teacher = Teacher.objects.get(username=self.request.user)
+        return teacher
+
+    def get_current_exam(self):
+        exam_id = self.request.data['exam']
+        exam = Exam.objects.get(id=exam_id)
+        return exam
+
+    def filter_queryset(self, queryset):
+        return queryset.filter(owner=self.get_current_teacher())
+
+    def perform_create(self, serializer):
+        return serializer.save(exam=self.get_current_exam(), owner=self.get_current_teacher())
