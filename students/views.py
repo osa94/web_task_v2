0a3275af90a90b1ea00c79ca9956a4396628808d
Oskar <oskarkazmierczak1994@gmail.com>
2019-05-14 20:57:35 +0200

    Create StudentViewSet
    Created StudentViewSet in students/views

--- students/views.py
+++ students/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from students.models import Student
+from students.serializers import StudentSerializer
+from students.permissions import ReadOnly
 
-# Create your views here.
+
+class StudentViewSet(viewsets.ModelViewSet):
+
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = (ReadOnly, )
