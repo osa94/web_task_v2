d3051179d78f92384d834d4270736fe077a73c20
Oskar <oskarkazmierczak1994@gmail.com>
2019-05-14 19:47:15 +0200

    Create model Teacher
    Created model Teacher in teachers/models

--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Teacher(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+
+    def __str__(self):
+        return self.username.username
